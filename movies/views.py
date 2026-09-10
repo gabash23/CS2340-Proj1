9a1476bc70bab8ee12a4ed390b532ef5267c954d
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2026-09-09 21:52:30 -0400

    added final user story

--- movies/views.py
+++ movies/views.py
@@ -61,3 +61,8 @@ def delete_review(request, id, review_id):
         user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
+
+def flag_and_delete(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    review.delete()
+    return redirect('movies.show', id=id)
